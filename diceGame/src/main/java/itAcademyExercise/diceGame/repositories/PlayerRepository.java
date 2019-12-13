b64e04893d4d1d70b6d1db9154c66915fe00657b
Ali Elias <alitunja27@gmail.com>
2019-12-13 12:24:49 +0100

    All working fine, just need to change to Auto generated ID not to overlap others users.

---
+++ diceGame/src/main/java/itAcademyExercise/diceGame/repositories/PlayerRepository.java
@@ -0,0 +1,10 @@
+package itAcademyExercise.diceGame.repositories;
+
+import itAcademyExercise.diceGame.domain.Player;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PlayerRepository extends CrudRepository<Player, Integer> {
+}
