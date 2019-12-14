b0a66c19dd937a05957834ca14338fdcd274bcd5
Ali Elias <alitunja27@gmail.com>
2019-12-14 16:20:43 +0100

    Deleting other folder

--- diceGameFinal/src/main/java/itAcademyExercise/diceGame/repositories/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package itAcademyExercise.diceGame.repositories;
-
-import itAcademyExercise.diceGame.domain.Player;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerRepository extends CrudRepository<Player, Integer> {
-}
